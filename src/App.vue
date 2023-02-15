d168eb2fda09935f1868bb4af62f895a258648ab
Alexander Loshakov <alevas556@icloud.com>
2023-02-15 11:21:15 +0300

    Добавлен TextField и кнопка добавления контакта

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,47 @@
     <v-app id="inspire">
         <NavBar />
         <v-main>
-            <h1>Phone Book App</h1>
+            <v-container fluid>
+                <v-row>
+                    <v-col cols="12">
+                        <v-card max-width="600" class="mx-auto">
+                            <v-toolbar color="teal-lighten-1" dark>
+                                <TextField />
+                                <v-btn icon class="ml-5" @click="createContact">
+                                    <v-icon>mdi-plus</v-icon>
+                                </v-btn>
+                            </v-toolbar>
+                            <h1>Phone Book App</h1>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-main>
     </v-app>
 </template>
 
 <script>
 import NavBar from '@/components/nav/NavBar.vue';
+import TextField from '@/components/ui/TextField.vue';
 
 export default {
     components: {
-        NavBar
+        NavBar,
+        TextField
     },
     data() {
         return {};
+    },
+    methods: {
+        createContact() {
+            console.log('creating contact...');
+        }
     }
 };
 </script>
+
+<style lang="scss">
+.v-toolbar__content {
+    padding: 4px 20px;
+}
+</style>
---
+++ src/components/ui/TextField.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-text-field
+        density="compact"
+        variant="solo"
+        clearable
+        single-line
+        hide-details
+        prepend-inner-icon="mdi-text-box-search-outline"
+        label="Search a contact"
+        placeholder="Search a contact"
+        :value="enteredValue"
+        @input="setSearch"
+    ></v-text-field>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            enteredValue: ''
+        };
+    },
+    methods: {
+        setSearch(evt) {
+            const value = evt.target.value;
+            this.enteredValue = value;
+            console.log(value);
+        }
+    }
+};
+</script>
+
+<style></style>
