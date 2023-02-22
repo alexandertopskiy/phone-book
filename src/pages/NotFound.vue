55c9bbcffa2878dd5c233482e21a5dc285c4f166
Alexander Loshakov <alevas556@icloud.com>
2023-02-22 19:11:26 +0300

    Обработка несуществующего маршрута

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-main class="bg-teal-lighten-3 d-flex flex-column justify-center">
+        <v-container fluid class="text-center">
+            <v-card max-width="600" class="py-10 mx-auto">
+                <h1 class="my-4">Page not found</h1>
+                <p class="my-2">This page doesn't exist yet</p>
+                <p class="my-2">
+                    Maybe you wanna check
+                    <router-link to="/contacts">contacts list?</router-link>
+                </p>
+            </v-card>
+        </v-container>
+    </v-main>
+</template>
+
+<style scoped lang="scss">
+a {
+    text-decoration: none;
+    color: #26a69a; // teal-lighten-1
+
+    &:hover,
+    &:active {
+        color: #b2dfdb; // teal-lighten-4
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import ContactsPage from '@/pages/ContactsPage.vue';
+import NotFound from '@/pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/contacts' },
-        { path: '/contacts', component: ContactsPage }
+        { path: '/contacts', component: ContactsPage },
+        { path: '/:notFound(.*)', component: NotFound }
     ]
 });
 
