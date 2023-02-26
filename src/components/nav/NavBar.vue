ad17afa06843c7df59f6de06b09dd2ec723dc28c
Alexander Loshakov <alevas556@icloud.com>
2023-02-26 13:29:54 +0300

    Обновление NavBar для разных состояний

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,19 +1,20 @@
 <template>
-    <div>
-        <v-app-bar color="teal-lighten-1">
-            <v-app-bar-nav-icon @click="toggleDrawerVisibility"></v-app-bar-nav-icon>
-            <!-- TODO: делать по центру для десктопа, прижимать к левому краю для планшетов, скрывать для мобилок -->
-            <v-toolbar-title class="hidden-sm-and-down">PhoneBook</v-toolbar-title>
-            <!-- v-if="userName" - чтобы не было блика при логауте -->
-            <h3 class="ms-4" v-if="userName">Hello, {{ userName }}!</h3>
-            <v-spacer></v-spacer>
-            <v-btn @click="logout">Logout</v-btn>
-        </v-app-bar>
-        <!-- Боковое Меню -->
-        <v-navigation-drawer v-model="drawer">
-            <MenuList @close-menu="toggleDrawerVisibility" />
-        </v-navigation-drawer>
-    </div>
+    <v-app-bar color="teal-lighten-1" v-if="isAuth">
+        <v-app-bar-nav-icon @click="toggleDrawerVisibility" class="icon"></v-app-bar-nav-icon>
+        <v-toolbar-title class="hidden-sm-and-down">PhoneBook</v-toolbar-title>
+        <!-- v-if="userName" - чтобы не было блика при логауте -->
+        <h3 class="ms-md-4 text-subtitle-1">Hello, {{ userName }}!</h3>
+        <v-spacer></v-spacer>
+        <v-btn @click="logout">Logout</v-btn>
+    </v-app-bar>
+    <v-app-bar color="teal-lighten-1" v-else>
+        <v-toolbar-title class="text-center">PhoneBook</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- Боковое Меню -->
+    <v-navigation-drawer v-model="drawer" v-if="isAuth">
+        <MenuList @close-menu="toggleDrawerVisibility" />
+    </v-navigation-drawer>
 </template>
 
 <script>
@@ -32,6 +33,9 @@ export default {
     computed: {
         userName() {
             return this.$store.getters.userMail;
+        },
+        isAuth() {
+            return this.$store.getters.isAuth;
         }
     },
     methods: {
