7cea93b7a729c6804a64adb57ab68930e13ac7fe
Alexander Loshakov <alevas556@icloud.com>
2023-02-26 16:02:01 +0300

    Добавлен v-tooltip для контакта вместо title

--- src/components/ui/CopiedButton.vue
+++ src/components/ui/CopiedButton.vue
@@ -1,6 +1,7 @@
 <template>
-    <v-btn @click="copyToClipboard(data)" variant="tonal" title="Click to Copy to Clipboard">
-        <strong>{{ title }}: </strong> {{ data }}
+    <v-btn @click="copyToClipboard(data)" variant="tonal">
+        <strong class="mr-1">{{ title }}: </strong> {{ data }}
+        <v-tooltip activator="parent" location="bottom">Click to Copy to Clipboard</v-tooltip>
     </v-btn>
 </template>
 
